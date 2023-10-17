4e15ce80593ac444a312a71c39f661745db555cc
Deepak Kushwaha <148253906+dkushwaha2543@users.noreply.github.com>
2023-10-17 21:38:33 +0530

    array element change

--- src/Array/Array1.java
+++ src/Array/Array1.java
@@ -7,7 +7,7 @@ public class Array1 {
             System.out.print(arr[i]+" ");
         }*/
         int [][] arr= {{1,2,3},
-                     {4,5,6},
+                     {4,5,7},
                      {7,8,9}};
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr.length;j++){
