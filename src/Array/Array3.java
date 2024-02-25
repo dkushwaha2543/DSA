bdc37eef45b8c2be9728afd20be63cbc26e567b7
deepak <deepakkushwaha2543@gmail.com>
2024-02-25 20:44:42 +0530

    Merging two sorted Array in one array

--- src/Array/Array3.java
+++ src/Array/Array3.java
@@ -19,7 +19,7 @@ public class Array3 {
 
             }
         }
-            System.out.println("Entered elemente are");
+            System.out.println("Entered elements are");
             for (int i=0;i<arr.length;i++){
                 for (int j=0;j<arr[i].length;j++){
                     System.out.print(arr[i][j]+" ");
